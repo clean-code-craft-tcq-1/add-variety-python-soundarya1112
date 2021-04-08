72c5d5861cef73ab6d2309387be6a78c9882380d
soundarya1112 <74286544+soundarya1112@users.noreply.github.com>
2021-04-08 14:18:01 +0530

    Update typewise_alert.test.py

--- typewise_alert.test.py
+++ typewise_alert.test.py
@@ -10,9 +10,7 @@ class TypewiseTest(unittest.TestCase):
   
   def test_check_and_alert(self):
     self.assertFalse(typewise_alert.check_and_alert('TO_EMAIL', 'PASSIVE_COOLING', 80) == 'Hi, the temperature is too high')
-    self.assertFalse(typewise_alert.check_and_alert('TO_CONTROLLER', 'PASSIVE_COOLING', 80) == '55071, TOO_HIGH')
+   
       
-
-
 if __name__ == '__main__':
   unittest.main()
