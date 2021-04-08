a5b8208bf17f0c6a0eed38c3f3c18100d0a9843e
soundarya1112 <74286544+soundarya1112@users.noreply.github.com>
2021-04-08 13:49:08 +0530

    Update typewise_alert.test.py

--- typewise_alert.test.py
+++ typewise_alert.test.py
@@ -5,6 +5,12 @@ import typewise_alert
 class TypewiseTest(unittest.TestCase):
   def test_infers_breach_as_per_limits(self):
     self.assertTrue(typewise_alert.infer_breach(20, 50, 100) == 'TOO_LOW')
+    self.assertTrue(typewise_alert.infer_breach(80, 20, 70) == 'TOO_HIGH')
+    self.assertTrue(typewise_alert.infer_breach(50, 20, 60) == 'NORMAL')
+  
+  def test_check_and_alert(self):
+    self.assertTrue(typewise_alert.check_and_alert('TO_CONSOLE',{'coolingType':'HIGH_ACTIVE_COOLING'},80)== True)
+    self.assertTrue(typewise_alert.check_and_alert('TO_EMAIL',{'coolingType':'HIGH_ACTIVE_COOLING'},80)== True)  
 
 
 if __name__ == '__main__':
